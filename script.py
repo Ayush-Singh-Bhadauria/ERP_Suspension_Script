2431c47390a4dedd6b61d491688f9aef736d4d16
Ayush Singh Bhadauria <132375737+Ayush-Singh-Bhadauria@users.noreply.github.com>
2024-04-02 09:42:08 +0530

    Update script.py
    for running temporarily

--- script.py
+++ script.py
@@ -14,31 +14,35 @@ Firefox_options.add_argument('--no-sandbox')  # Bypasses OS security model ( ais
 browser = webdriver.Firefox(options = Firefox_options) # new firefox webdriver instance created successfully
 
 def function():
-    counter = 0 # counter for while loop
+    # counter = 0 # counter for while loop
     while counter < 6:
         url = "https://erp.psit.ac.in/"
         browser.get(url) # receives url
-
-        username_input = browser.find_element(By.NAME, 'username') # login button find using the name attribute
+        
+        # Wait for the username input field to be present
+        username_input = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.NAME, 'username'))
+        )
+        # username_input = browser.find_element(By.NAME, 'username') # login button find using the name attribute
         password_input = browser.find_element(By.NAME, 'password') # password button find using the name attribute
 
-        username_input.send_keys('2201640100114') #Value that needs to be inside the username field
+        username_input.send_keys('2201640100105') #Value that needs to be inside the username field
         password_input.send_keys('845135688525') #value for password field
 
         login_button = browser.find_element(By.CLASS_NAME, 'btn-lg')# Locate and click the "Log in" button by Class Name attribute because only name attribute was not found
         login_button.click()
 
-        time.sleep(3) # thodi der ruk jaane ke liye
-        counter += 1
+       # time.sleep(3) # thodi der ruk jaane ke liye
+       # counter += 1
 
 
 def run_every_15_minutes():
     while True:
         # Call your function
         function()
-
+        #below code prints time
+        current_time = now.strftime("%H:%M:%S")
+        print("finsihed on =", current_time)
         # Pause the execution for 15 minutes
         time.sleep(16 * 60)  # 15 minutes * 60 seconds/minute = 900 seconds
-
-browser.quit() #quits the instance
 
